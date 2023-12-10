240d2055174a1447aef0e811b9f272f200b628ee
Charlene Orr <charlorrnot@gmail.com>
2023-12-10 05:37:56 -0600

    Seperated about and summary into pages

---
+++ script.js
@@ -0,0 +1,13 @@
+// Controls DOM manipulation on button clicks
+
+document.getElementById('to-about').addEventListener('click',function(){
+
+    document.getElementById('summary').style.display = "none";
+    document.getElementById('about').style.display = "block";
+});
+
+document.getElementById('to-summary').addEventListener('click',function(){
+
+    document.getElementById('about').style.display = "none";
+    document.getElementById('summary').style.display = "block";
+});
