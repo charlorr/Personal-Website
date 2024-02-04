70f015010e22e941427feccbe86f3c635bd3130b
Charlene Orr <charlorrnot@gmail.com>
2024-02-04 03:56:35 -0600

    Fix page overflow

--- script.js
+++ script.js
@@ -2,12 +2,27 @@
 
 document.getElementById('to-about').addEventListener('click',function(){
 
-    document.getElementById('summary').style.display = "none";
-    document.getElementById('about').style.display = "block";
+    window.location.hash = '#about';
+    route()
 });
 
 document.getElementById('to-summary').addEventListener('click',function(){
 
-    document.getElementById('about').style.display = "none";
-    document.getElementById('summary').style.display = "block"; 
+    window.location.hash = '#home'
+    route()
 });
+
+let route = () => {
+    hash = window.location.hash
+
+    if (hash === '' || hash === '#' || hash === '#home') {
+        document.getElementById('about').style.display = "none";
+        document.getElementById('summary').style.display = "block"; 
+    }
+    else if (hash === '#about') {
+        document.getElementById('summary').style.display = "none";
+        document.getElementById('about').style.display = "block";
+    }
+}
+
+route()
